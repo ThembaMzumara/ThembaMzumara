7474ef88d99a984dfc6cc89c5caf973cd7c6061b
Themba <themba@stratisglobal.com>
2024-11-16 09:30:44 +0200

    Style: restyiled the layout of the card with better center position for card

--- web_platform/src/pages/index.tsx
+++ web_platform/src/pages/index.tsx
@@ -2,7 +2,9 @@ import { FaTwitter, FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col items-center justify-center h-[400px] px-4 bg-[hsla(0, 0%, 68%, 0.812)] shadow-md rounded-md max-w-[500px] mx-auto my-20">
+
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="relative flex flex-col items-center justify-center h-[300px] px-4 bg-[hsla(0, 0%, 68%, 0.812)] shadow-md rounded-md max-w-[450px]">
       {/* Image Section */}
       <div className="absolute -top-12">
         <img
@@ -11,7 +13,7 @@ export default function Home() {
           className="w-24 h-24 sm:w-32 sm:h-32 rounded-full shadow-lg border-4 border-white object-cover"
         />
       </div>
-
+      
       {/* Bio and Links Section */}
       <div className="flex flex-col items-center mt-16 px-4">
         {/* Bio Text */}
@@ -21,7 +23,7 @@ export default function Home() {
             on exciting MERN and .NET projects. Happy to connect and refer!
           </p>
         </div>
-
+      
         {/* Social Links */}
         <div className="flex space-x-6">
           <a
@@ -58,6 +60,10 @@ export default function Home() {
           </a>
         </div>
       </div>
-    </div>
+
+  </div>
+</div>  
+
+    
   );
 }
